76e8f52e7ed0415fb8aebee4283e5663cee0f67c
Rick <ricardobriangarcia@gmail.com>
2024-08-08 14:55:41 -0300

    First Finalizado

---
+++ First.tab/Contagem.panel/Luvas.pushbutton/script.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+__title__ = "Parametro das luvas aninhadas"                                       # Name of the button displayed in Revit UI
+__doc__ = """Version = 1.0
+Date    = 20.04.2022
+_____________________________________________________________________
+Description:
+This is a template file for pyRevit Scripts.
+_____________________________________________________________________
+How-to: (Example)
+-> Click on the button
+-> Change Settings(optional)
+-> Make a change
+_____________________________________________________________________
+Last update:
+- [12.06.2023] - 1.1 UPDATE - New Feature
+- [12.06.2023] - 1.0 RELEASE
+_____________________________________________________________________
+To-Do:
+- Check Revit 2021
+- Add ... Feature
+_____________________________________________________________________
+Author: Erik Frits"""                                           # Button Description shown in Revit UI
+
+# pyRevit EXTRA metatags: You can remove them.
+__author__ = "Erik Frits"                                       # Script's Author
+__helpurl__ = "https://www.youtube.com/watch?v=YhL_iOKH-1M"     # Link that can be opened with F1 when hovered over the tool in Revit UI.
+# __highlight__ = "new"                                         # Button will have an orange dot + Description in Revit UI
+__min_revit_ver__ = 2022                                        # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
+__max_revit_ver = 2024                                          # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
+# __context__     = ['Walls', 'Floors', 'Roofs']                # Make your button available only when certain categories are selected. Or Revit/View Types.
+# __context__     = ['Walls', 'Floors', 'Roofs']                # Make your button available only when certain categories are selected. Or Revit/View Types.
+# Docs Link: https://pyrevitlabs.notion.site/Anatomy-of-IronPython-Scripts-f11d0099667f46a28d29b028dd99ccaf
+
+# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
+# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
+# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
+# ==================================================
+# Regular + Autodesk
+import os, sys, math, datetime, time                                    # Regular Imports
+from Autodesk.Revit.DB import Transaction, FilteredElementCollector, BuiltInCategory     # or Import only classes that are used.
+# from Autodesk.Revit.DB.Architecture import Room, TopographySurface      # Import Discipline Specific Elements
+from Autodesk.Revit.DB import *                                         # Import everything from DB (Very good for beginners)
+
+# pyRevit
+from pyrevit import revit, script, forms                                        # import pyRevit modules. (Lots of useful features)
+
+# Custom Imports
+from Snippets._selection import get_selected_elements                        # lib import
+from Snippets._convert   import convert_internal_to_m                        # lib import
+from Snippets._inserir_parametro_aninhado import inserir_parametro_aninhado  # lib import
+
+# .NET Imports
+import clr                                  # Common Language Runtime. Makes .NET libraries accessinble
+clr.AddReference("System")                  # Refference System.dll for import.
+from System.Collections.Generic import List # List<ElementType>() <- it's special type of list from .NET framework that RevitAPI requires
+# List_example = List[ElementId]()          # use .Add() instead of append or put python list of ElementIds in parentesis.
+
+
+
+
+
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
+# ==================================================
+doc       = __revit__.ActiveUIDocument.Document   #type: UIDocument     # Document   class from RevitAPI that represents project. Used to Create, Delete, Modify and Query elements from the project.
+uidoc     = __revit__.ActiveUIDocument            #type: Document       # UIDocument class from RevitAPI that represents Revit project opened in the Revit UI.
+selection = uidoc.Selection                       #type: Selection
+app       = __revit__.Application                 #type: UIApplication  # Represents the Autodesk Revit Application, providing access to documents, options and other application wide data and settings.
+rvt_year  = int(app.VersionNumber)                # e.g. 2023
+
+active_view  = doc.ActiveView
+active_level = active_view.GenLevel           # Only FloorPlans are associated with a Level!
+PATH_SCRIPT  = os.path.dirname(__file__)      # Absolute path to the folder where script is placed.
+
+# GLOBAL VARIABLES
+# - Place global variables here.
+
+# Selecionando a categoria conexoes de tubo e filtrando para nao selecionar os tipos, apenas instancias
+conexoesDeTubo = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_PipeFitting).WhereElementIsNotElementType()
+nomeParametroAninhado = "Abreviatura do Sistema"
+nomeParametroHospedeiro = "Abreviatura do Sistema"
+
+# ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+# ╠╣ ║ ║║║║║   ║ ║║ ║║║║╚═╗
+# ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝╚═╝ FUNCTIONS
+# ==================================================
+# - Place local functions here. If you might use any functions in other scripts, consider placing it in the lib folder.
+
+
+
+# ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗╔═╗
+# ║  ║  ╠═╣╚═╗╚═╗║╣ ╚═╗
+# ╚═╝╩═╝╩ ╩╚═╝╚═╝╚═╝╚═╝ CLASSES
+# ==================================================
+
+# - Place local classes here. If you might use any classes in other scripts, consider placing it in the lib folder.
+
+# ╔╦╗╔═╗╦╔╗╔
+# ║║║╠═╣║║║║
+# ╩ ╩╩ ╩╩╝╚╝ MAIN
+# ==================================================
+if __name__ == '__main__':
+
+    # START CODE HERE
+
+
+    # AVOID  placing Transaction inside of your loops! It will drastically reduce perfomance of your script.
+    t = Transaction(doc,__title__)  # Transactions are context-like objects that guard any changes made to a Revit model.
+
+    # You need to use t.Start() and t.Commit() to make changes to a Project.
+    t.Start()  # <- Transaction Start
+    #- CHANGES TO REVIT PROJECT HERE
+
+    inserir_parametro_aninhado(conexoesDeTubo, nomeParametroHospedeiro, nomeParametroAninhado)
+    
+    t.Commit()  # <- Transaction End
+
+    # Notify user that script is complete.
+    print('-' * 50)
+    print('Script is finished.')
+    print('Template has been developed by Erik Frits.')
---
+++ lib/Snippets/_inserir_parametro_aninhado.py
@@ -0,0 +1,21 @@
+
+from Autodesk.Revit.DB import Transaction
+
+def inserir_parametro_aninhado(instancias, nomeParametroHospedeiro, nomeParametroAninhado):
+
+    contador = 0
+    for elemento in instancias:
+        if elemento.SuperComponent():
+            elementoAninhado = elemento
+            elementoHospedeiro = elemento.SuperComponent
+
+    parametroHospedeiro = elementoHospedeiro.LookupParameter(nomeParametroHospedeiro)
+    if parametroHospedeiro:
+        valorParametroHospedeiro = parametroHospedeiro.asString()
+
+    parametroAninhado = elementoAninhado.LookupParameter(nomeParametroAninhado)
+    if parametroAninhado:
+        parametroAninhado.Set(valorParametroHospedeiro)
+    contador += 1
+
+    return contador
